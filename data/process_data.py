6eeaf10049493491a2bd5f9b57ce3d5f4aa9e173
karim-at-birds <karim@birdsonmars.com>
2023-03-10 17:41:26 +0000

    feat:added etl pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,46 @@
+# import libraries
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+# load messages dataset
+    messages = pd.read_csv(messages_filepath)
+# load categories dataset
+    categories = pd.read_csv(categories_filepath)
+# merge datasets
+    df = messages.merge(categories, on="id")
+    return df
 
 def clean_data(df):
-    pass
+# create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(";",expand=True)
+# select the first row of the categories dataframe
+    row = categories.iloc[0,:]
 
+# use this row to extract a list of new column names for categories.
+# one way is to apply a lambda function that takes everything 
+# up to the second to last character of each string with slicing
+    category_colnames = row.str[:-2]
 
+# rename the columns of `categories`
+    categories.columns = category_colnames
+# converting to int
+    categories = categories.applymap(lambda x:int(x[-1]))
+
+# drop the original categories column from `df`
+    df.drop(columns="categories", inplace=True)
+
+# remove duplicates
+    df.drop_duplicates(inplace=True)
+
+# concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis=1)
+
+    return df
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('MainTable', engine, index=False)  
 
 
 def main():
