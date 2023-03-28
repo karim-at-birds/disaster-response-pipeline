982c0c1f9e12bfd37e410d249daee49874c83f67
karim-at-birds <karim@birdsonmars.com>
2023-03-28 19:18:33 +0300

    bugfix: MainTable is replaced if it already exists

--- data/process_data.py
+++ data/process_data.py
@@ -79,7 +79,7 @@ def save_data(df, database_filename):
     database_filename (str): File path to the SQLite database.
     """
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('MainTable', engine, index=False)  
+    df.to_sql('MainTable', engine, index=False, if_exists='replace')  
 
 
 def main():
